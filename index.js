9e72b72ed47f3da9ffb7213eaac0a4870de50912
Tony DiPasquale <anthony.r.dipasquale@gmail.com>
2020-03-15 15:19:32 -0700

    Tailor to my use case

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ require('dotenv').config()
 
 const { fetchTransactions } = require('./lib/fetch')
 const { transformTransactionsToUpdates } = require('./lib/transform')
-const { updateSheet } = require('./lib/update')
+const { updateSheet, fetchData } = require('./lib/update')
 
 ;(async () => {
   const transactions = await fetchTransactions()
-  const updates = transformTransactionsToUpdates(transactions)
+  const data = await fetchData();
+  const updates = transformTransactionsToUpdates(data, transactions)
   updateSheet(updates)
 })()
